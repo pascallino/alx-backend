414bee3c7234bce9932f7a54171d82aa7021e36b
pascallino <luvpascal.ojukwu@yahoo.com>
2024-02-06 12:16:52 +0100

    1-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,6 +25,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """
     Select and return best language match based on supported languages
@@ -33,9 +34,6 @@ def get_locale():
     if loc in app.config['LANGUAGES']:
         return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', strict_slashes=False)
